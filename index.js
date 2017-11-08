8a0d3d63f7e7d6ff2fb99a421eb707e5e62b4dcb
Rachel Feeken <rfeeken@gmail.com>
2017-11-08 01:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newArray = kittens.slice(0, array.length - 1);
+  var newArray = kittens.slice(0, kittens.length - 1);
   return newArray;
 }
